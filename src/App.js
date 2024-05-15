317491d3c70ba90d02d55d44ea2cfb9436269be3
Minh Nhut <lenhut0929@gmail.com>
2024-05-15 13:52:01 +0700

    update template react js

---
+++ src/App.js
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <div className="App">
+      <div className='Header'>
+        Header Content
+      </div>
+
+      <div className="Layout">
+        <div className="Layout_Sidebar">
+          Sidebar Content
+        </div>
+        <div className="Layout_Content">
+          Main Content
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
